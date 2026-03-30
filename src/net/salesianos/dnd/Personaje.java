8dfade47732c75819e75aa8cbbe0b1f80846b42a
Asier Fernández <asierferzam@gmail.com>
2026-03-30 17:50:48 +0000

    Atributos y constructor de Personaje

--- src/net/salesianos/dnd/Personaje.java
+++ src/net/salesianos/dnd/Personaje.java
@@ -1,5 +1,12 @@
 package net.salesianos.dnd;
 
 public class Personaje {
-    
+    String nombre;
+    int nivel;
+    int energia = 100;
+
+    public Personaje(String nombre, int nivel){
+        this.nombre = nombre;
+        this.nivel = nivel;
+    }
 }
