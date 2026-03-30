30d5389d8033c3073675feb48848d45661d606ab
Asier Fernández <asierferzam@gmail.com>
2026-03-30 19:21:43 +0000

    Creación de personaje y uso de sus métodos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import net.salesianos.dnd.Personaje;
+import net.salesianos.dnd.guerrero.Guerrero;
+import net.salesianos.dnd.mago.Mago;
 public class App {
     public static void main(String[] args) {
-        
+        Personaje viajero = new Personaje("Eldrin", 3);
+        viajero.entrenar(4);
+        viajero.gastarEnergia(30);
+        viajero.mostrarInfo();
+        Personaje.mostarTotalPersonajes();
     }
 }
