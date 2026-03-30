ca1911cb6d878348a64d9072918bd50bc67f6aa6
Asier Fernández <asierferzam@gmail.com>
2026-03-30 19:04:34 +0000

    Atributo y constructor de Mago

--- src/net/salesianos/dnd/mago/Mago.java
+++ src/net/salesianos/dnd/mago/Mago.java
@@ -1,5 +1,12 @@
 package net.salesianos.dnd.mago;
+import net.salesianos.dnd.Personaje;
+public class Mago extends Personaje{
+    String elemento;
 
-public class Mago {
+    public Mago(String nombre, int nivel, String elemento){
+        super(nombre, nivel);
+        this.elemento = elemento;
+    }
+
     
 }
